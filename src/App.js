8c2fc07e3fb5fae1fafa156392d7d1ba6974c7bd
gabibble <87248000+gabibble@users.noreply.github.com>
2022-02-16 18:28:27 -0800

    added html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
 
         <footer>
           This project was created by Julia Giebultowicz and is{" "}
