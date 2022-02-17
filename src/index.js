d8558bfbce51a208df464ba3f6dc06891ad9be9c
gabibble <87248000+gabibble@users.noreply.github.com>
2022-02-16 19:06:49 -0800

    started css

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 
 ReactDOM.render(
   <React.StrictMode>
