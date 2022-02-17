214024f82d383a4c9fe0b1c50572ac9db10b674d
gabibble <87248000+gabibble@users.noreply.github.com>
2022-02-16 17:40:27 -0800

    added botstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Julia Giebultowicz and is{" "}
-        <a
-          href="https://github.com/gabibble/practice.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was created by Julia Giebultowicz and is{" "}
+          <a
+            href="https://github.com/gabibble/practice.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
