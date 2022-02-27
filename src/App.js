0c7d6afc0d681bff3d302cbe30913dfeeca13ee0
gabibble <87248000+gabibble@users.noreply.github.com>
2022-02-27 12:01:32 -0800

    slight style changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./index.css";
 import Weather from "./Weather";
 
 function App() {
--- src/Weather.js
+++ src/Weather.js
@@ -53,11 +53,7 @@ export default function Weather(props) {
               />
             </div>
             <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
+              <input type="submit" value="Search" className="btn w-100 " />
             </div>
           </div>
         </form>
