fc0c82afd86a361d4345c6cdf87c1841beab00a6
gabibble <87248000+gabibble@users.noreply.github.com>
2022-02-27 11:46:39 -0800

    removed C conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  if (unit === "fahrenheit") {
-    return (
-      <span>
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
-        <span className="unit">
-          °F{" "}
-          <a href="/" onClick={showCelsius}>
-            | °C
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    let celsius = ((props.fahrenheit - 32) * 5) / 9;
-
-    return (
-      <span>
-        <span className="temperature">{Math.round(celsius)}</span>
-        <span className="unit">
-          {" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F |{" "}
-          </a>
-          °C
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span>
+      <span className="temperature">{Math.round(props.fahrenheit)}</span>{" "}
+      <span className="unit">°F</span>
+    </span>
+  );
 }
